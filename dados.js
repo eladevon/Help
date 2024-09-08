56ba8589db2fe21b237bbffd7bfdc950646f781d
Elaine Dev On Moura <eladevon@gmail.com>
2024-09-07 22:27:23 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,57 @@
+let dados = [
+    {
+        titulo : "Seca e Queimadas",
+        descricao : "Mantenha-se hidratado. Acompanhe os índices de umidade do ar.  Em caso de incêndio, ligue para o Corpo de Bombeiros.",
+        link: "https://brasilescola.uol.com.br/geografia/queimadas.htm#Poss%C3%ADveis+solu%C3%A7%C3%B5es+para+as+queimadas",
+        tags: "seca fogo incendio incêndio quentura abafado mormaço inferno hipertemia flama"    
+    },
+    {
+        titulo: "Frio Intenso",
+        descricao: "Use roupas adequadas. Mantenha a casa aquecida. Verifique se há vazamentos de gás. Acompanhe as condições climáticas.",
+        link: "https://www.lusiadas.pt/blog/prevencao-estilo-vida/saude-familia/15-conselhos-para-se-proteger-bem-frio#:~:text=Procure%20locais%20abrigados%20se%20a,%C3%A1gua%20para%20evitar%20a%20desidrata%C3%A7%C3%A3o.",
+        tags: "secura frio gelo hipotermia"  
+    },
+    {
+        titulo: "Conflitos e Protestos",
+        descricao: "Evite áreas de conflito. Mantenha-se informado sobre os acontecimentos. Não participe de atos violentos. Siga as orientações das autoridades.",
+        link: "https://almofadasdaflor.com.br/como-posso-proteger-minha-casa-contra-vandalismo/",
+        tags: "confusão vandalismo vandalos vândalos quebra-quebra guerra"  
+    },
+    {
+        titulo: "Violência contra a Mulher",
+        descricao: "Denuncie: Ligue 180 ou procure a delegacia mais próxima.",
+        link: "https://api.whatsapp.com/send?phone=5561996110100",
+        tags: "mulheres feminicidio feminicídio estupro surra taca agressão defesa"  
+    },
+    {
+        titulo: "Violência contra o Idoso",
+        descricao: "Denuncie: Ligue para o Disque 100 ou procure a delegacia mais próxima.",
+        link: "https://api.whatsapp.com/send?phone=5561996110100",
+        tags: "velhos velhas idosas coroa agressão estelionato"  
+    },
+    {
+        titulo: "Bullying",
+        descricao: "Procure ajuda: Converse com um professor, orientador ou responsável de confiança. Denuncie: Não tenha medo de falar sobre o que está acontecendo. Lembre-se: você não está sozinho.",
+        link: "https://www.ewcursos.com/blog/bullying-como-ajudar-alguem#:~:text=Reportar%20aos%20pais%20da%20v%C3%ADtima,culpada%20do%20que%20est%C3%A1%20acontecendo.",
+        tags: "escola alunos alunas professores professoras colégio faculdade instituo instituição ensino aprendizagem curso criança crianca adolescente jovem menina garota garoto menino"  
+    },
+    {
+        titulo: "Chuvas e Ventos fortes",
+        descricao: "Proteja-se: Fique em local seguro, longe de janelas.Procure abrigo: Em prédios ou carros. Desligue aparelhos: Evite curto-circuitos. Verifique a casa: Procure por problemas na estrutura. Evite áreas abertas: Áreas com árvores e postes são perigosas. Ao dirigir: Evite a estrada: Se possível, não dirija, senão redobre a atenção e evite áreas alagadas. Ligue para os bombeiros: 193 ou para a Defesa Civil.",
+        link: "https://cor.rio/recomendacoes-chuva-vento/",
+        tags: "alagamentos enchentes cheias rios furacão furacao tsunami tempestades niño nino niña nina ciclone alagamento extratropical rajadas redemoinhos tromba dágua águas"  
+    },
+    {
+        titulo: "Infarto",
+        descricao: "Dor intensa entre a boca e o umbigo que dura mais de 20 minutos, Suor frio, Falta de ar, Mal-estar. Ligue para os bombeiros: 193 ou para a Defesa Civil.",
+        link: "https://drauziovarella.uol.com.br/doencas-e-sintomas/infarto-do-miocardio-ataque-cardiaco/#:~:text=Uma%20das%20formas%20mais%20comuns,o%20esfor%C3%A7o%20e%20o%20estresse./",
+        tags: "dor coração coracao enfarto miocárdio infartando"  
+    },
+    {
+        titulo: "AVC",
+        descricao: "Ligar para o Serviço de Atendimento Médico de Urgência (SAMU - 192) ou Bombeiros (193). Levar a pessoa imediatamente a um hospital. Manter a calma e acalmar a pessoa. Deitar a pessoa de lado, com a cabeça ligeiramente elevada e apoiada. Não dar comida ou bebida para a pessoa",
+        link: "https://www.tuasaude.com/primeiros-socorros-para-avc/#google_vignette",
+        tags: "derrame cérebro cerebral cerebro cabeça cabeca"  
+    }
+];
+
